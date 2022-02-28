cfb76de39664f6e2f67e1f280670896671011d1e
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-02-28 02:51:33 +0100

    Update b64crypt.py

--- src/haunt/b64crypt.py
+++ src/haunt/b64crypt.py
@@ -68,9 +68,12 @@ def base64encode():
             elif hash_value == 'c':
                 os.system('clear')
                 return base64encode()
-                        
+                  
+            ## Encode the contents of the binary input 
             m_bytes = choice.encode('ascii')
+            ## Encode the bytes-like object using Base64 and return the encoded bytes.
             b64_e = base64.b64encode(m_bytes)
+            ## Decode the contents of the binary input
             b64_hash = b64_e.decode('ascii')
             print(b64_hash)
             return chse()  
@@ -96,9 +99,12 @@ def base64encode():
                 elif b64_hash_val == 'c':
                     os.system('clear')
                     return b64_decrypt()
-                           
+                
+                ## Encode the contents of the binary input 
                 b64_d = b64_hash_val.encode('ascii')
+                ## Decode the Base64 encoded bytes-like object or ASCII string and return the decoded bytes.
                 m_bytes = base64.b64decode(b64_d)
+                ## Decode the contents of the binary input
                 result = m_bytes.decode('ascii')
                 print(result)
                 return chse()
