afedd3cf97f173848aa144e34ec13a7d140c2046
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-03-01 18:05:42 +0100

    Update hunter.py

--- hunter.py
+++ hunter.py
@@ -146,7 +146,7 @@ def HUNT3R():
                         try:
                             for port in range(port_min, port_max + 1):
                                 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: ## sock_DGRAM/UPD
-                                    s.settimeout(0.5) 
+                                    ## s.settimeout(0.5) 
                                     s.connect((target_ip, port))
                                     open_ports.append(port)
                               
