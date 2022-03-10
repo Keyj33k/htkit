bb853b82562b04acefc55ace1a49505b96db4274
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-03-10 01:31:45 +0100

    Delete banner_whereareyou.py

--- banner_whereareyou.py
+++
@@ -1,9 +0,0 @@
-def set_help_banner():
-    print(colored("""
- HELP; WhoAreYou
-< ============= >
-'/x' or '/exit'        Return Menu / Exit WhoAreYou
-'/c' or '/clear'       Clear Screen             
-    """, "yellow"))
-    print(chr(0xa))
-    input("Press any key ...")
