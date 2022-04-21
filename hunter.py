840e3c246a7b9982e325c978519497012bb87df2
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-04-21 23:20:15 +0200

    Update hunter.py

--- hunter.py
+++ hunter.py
@@ -28,6 +28,11 @@ import os
 class Hunter:
 
     def banner(): 
+        magenta = "\033[0;35m"
+        yellow = "\033[0;33m"
+        green = "\033[0;32m"
+        cyan = "\033[0;36m"
+        red = "\033[0;31m"
         print(cld("""
     █    █                 █
     █    █                 █
@@ -38,79 +43,84 @@ class Hunter:
     █    █ █   █  █   █    █    █       █
     █    █ █▒ ▓█  █   █    █░   ▓▓  █   █
     █    █ ▒██▒█  █   █    ▒██   ███▒   █
-   
-    <by@keyjeek>  |  Follow the white rabbit... 
-    <contact:nomotikag33n@gmail.com>           
-    [i] Hunter is a small toolkit to perform information gathering.              
+               
         """, "cyan"))
+        print(magenta + " <" + green + " by@keyjeek " + magenta + ">" + cyan + " | " + green + "Follow the white rabbit ...") 
+        print(magenta + " <" + green + "contact:nomotikag33n@gmail.com" + magenta + ">")           
+        print(cyan + "\n[" + red + "i" + cyan + "]" + yellow + "Hunter is a small toolkit to perform information gathering.")   
 
     def menu():
-        print(cld("""
-    [0] Clear Screen
-    [1] Witcher
-    [2] MD5Crypt
-    [3] IPEye
-    [4] BannerGrabber
-    [5] B64Crypt
-    [6] PhoneStalk
-    [7] SubdomainScanner
-    [99] Exit
-        """, "yellow"))
+        yellow = "\033[0;33m"
+        cyan = "\033[0;36m"
+        red = "\033[0;31m"
+        print(magenta + "=" * 70)
+        print(cyan + "\n[" + red + "0" + cyan + "]" + yellow + " Clear Screen")
+        print(cyan + "[" + red + "1" + cyan + "]" + yellow + " Witcher\t\t A simple port scanner.")
+        print(cyan + "[" + red + "2" + cyan + "]" + yellow + " MD5Crypt\t\t MD5 encryption.")
+        print(cyan + "[" + red + "3" + cyan + "]" + yellow + " IPEye\t\t Get informations about an ip address.")
+        print(cyan + "[" + red + "4" + cyan + "]" + yellow + " BannerGrabber\t Get service behind a port.")
+        print(cyan + "[" + red + "5" + cyan + "]" + yellow + " B64Crypt\t\t En- and Decryption used base64.")
+        print(cyan + "[" + red + "6" + cyan + "]" + yellow + " PhoneStalk\t\t Get informations about a phonenumber")
+        print(cyan + "[" + red + "7" + cyan + "]" + yellow + " SubdomainScanner\t Get subdomains from any url.")
+        print(cyan + "[" + red + "99" + cyan + "]" + yellow + " Exit")
 
     def witcher():  
-        import socket
-        print(chr(0xa))
-        witcher_banner = pfgt.figlet_format("welcome", font="banner3-D")
-        print(cld(witcher_banner, "cyan"))
-        import time
-        time.sleep(1.05)
+        magenta = "\033[0;35m"
+        yellow = "\033[0;33m"
+        green = "\033[0;32m"
+        cyan = "\033[0;36m"
+        red = "\033[0;31m"
 
+        import socket
         os.system("clear")
         witcher_banner = pfgt.figlet_format("witcher", font="banner3-D")
         print(cld(witcher_banner, "cyan"))
-        print("*" * 18, "A python portscanner project ...", "*" * 18)
-        print("*" * 24, " *writtenby@Keyj33k ", "*" * 24)
+        print(magenta + "#" * 17 + green, " A python portscanner project ... " + magenta, "#" * 17)
+        print("#" * 24 + green, " *writtenby@Keyj33k " + magenta, "#" * 24 + "\n")
 
         try:
-            print(cld("\nTarget ~#:", "yellow"))
-            target_address = input()
-            
+            target_address = str(input(yellow + "Target (Exit with 'exit' or 'x'): " + red))
             if target_address == 'exit' or target_address == 'x':
                 return Hunter()
-            
         except ValueError:
             print(cld("\nYou need to enter a string!", "red"))
-            input("\nPress any key ...")
+            input(cyan + "\nPress any key ...")
 
-        print("-" * 70)
+        try:
+            target_port = int(input(yellow + "Max. Port (exit with '0'): " + red))
+            if target_port == 'exit' or target_port == 'x':
+                return Hunter()
+        except ValueError:
+            print(cld("\nYou need to enter a integer!", "red"))
+            input(cyan + "\nPress any key ...")
+
+        print(magenta + "=" * 70)
         scan_start = dtt.now()
-        print(cld(f"Started scanning at:\t\t\t{scan_start}", "green"))
-        print("-" * 70)
+        print(green + f"Started scanning at:\t\t\t{scan_start}")
+        print(magenta + "=" * 70)
         time_start = dtt.now()
 
         try:
-            for target in range(1, 65535):
+            for target in range(1, target_port):
                 socket_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
                 final_result = socket_sock.connect_ex((target_address, target)) 
                 socket_sock.settimeout(1) 
-                
                 if final_result == 0: 
-                    print("Port\t{}\t\t\t\t\t   open".format(target))
+                    print(green + "Port\t{}\t\t\t\t\t   open".format(target))
                 socket_sock.close() 
-
         except socket.error as socket_error:
             print(cld(socket_error, "red"))
-
         except KeyboardInterrupt:
             print(cld("\nCtrl+C pressed. Exit.", "red"))
             return Hunter()
 
         time_stop = dtt.now() 
         needed_time = time_stop - time_start
-        print("-" * 70)
-        print(cld(f"\nScanner done in {needed_time}!", "green"))
+        print(magenta + "=" * 70)
+        print(cld(f"Scanner done in {needed_time}!", "green"))
+        print(magenta + "=" * 70)
         print(chr(0xa))
-        input("Press any key ...")
+        input(cyan + "Press any key ...")
 
     def subdomain_scanner():
         sds_banner = pfgt.figlet_format("Sub- domain- Scanner", font = "banner3-D")
@@ -118,20 +128,16 @@ class Hunter:
 \n<by@keyjeek>  |  Follow the white rabbit...
 <contact:nomotikag33n@gmail.com> 
         """)
-        
         found_subdomain = []
         target_address = input("Target ~#: ")
-
         if target_address == 'exit' or target_address == 'x':
             return Hunter()
 
         with open("subdomains.txt") as FILE:
             read_file = FILE.read()
             SUBDOMAIN = read_file.splitlines()
-
             for list_domains in SUBDOMAIN:
                 UniformResourceLocator = f"http://{list_domains}.{target_address}"
-
                 try:
                     import requests
                     requests.get(UniformResourceLocator)
@@ -155,7 +161,6 @@ class Hunter:
             
             def encrypt(): 
                 hash_value = input("Text ~#: ")  
-                
                 if hash_value == 'exit' or hash_value == 'x':
                     print(cld("Exit", "red"))
                     return Hunter()    
@@ -171,22 +176,51 @@ class Hunter:
                     input("\nPress any key ...")
                     os.system('clear')
                     return Hunter()
-                
             encrypt()
                     
             def decrypt():
                 question_brute = input("Decrypt/Bruteforce the value? y/n ~#: ")
-                
                 if question_brute == 'y' or question_brute == 'Y':
                     print("USE THIS LINK: https://www.md5online.org/md5-decrypt.html ") # This program is using a big database to bruteforce the hash for you
                     input("\nPress any key ...")
                     return Hunter()     
                 elif question_brute == 'n' or question_brute == 'N':
                     return Hunter()
-                
             decrypt()
-            
         md5()
+
+    def conditions():
+        os.system("clear")
+        yellow = "\033[0;33m"
+        cyan = "\033[0;36m"
+        print(cld("""
+ \t\t\tWelcome To Hunter Toolkit!
+ < =============================================================================== >
+ \n Please note that actions like portscanning etc. can be illegal. 
+ If you want to use this tool, follow the conditions:
+
+ - This tool is made for educational purposes only.
+ - I'm not responsible for your actions.
+ - With great force follows great responsibility.
+
+ If you accept the conditions type 'y' and 'n' for decline.
+ Thank you and have a nice day!
+
+ ~ Keyjeek\n
+        """, "red"))
+
+        choice = input(yellow + "$ " + cyan)
+        if choice == 'y' or choice == 'Y':
+            pass
+        elif choice == 'n' or choice == 'N':
+            print(cld("Exit", "red"))
+            sys.exit(0)
+        else:
+            os.system("clear")
+            print(cld("Invalid Input!", "red"))
+            input(cyan + "Press any key to continue")
+            os.system("clear")
+            return Hunter.conditions()
 
     def eye_main():
         def ipeye():
@@ -198,14 +232,11 @@ class Hunter:
    --> some information about an IP.  
 [i] Type 'exit' to exit ipeye.
             """)
-                
             print(chr(0xa))
             ipeye_scanner = input("IPEye Target ~#: ")     
-            
             if ipeye_scanner == 'exit' or ipeye_scanner == 'x':
                 print(cld("Exiting ...", "red"))
                 return Hunter()      
-
             time_start = dtt.now()
 
             try:
@@ -227,16 +258,13 @@ class Hunter:
             print(chr(0xa))
             input("Press any key ...")
             return Hunter()
-        
         ipeye()
 
     def banner_grabber():
         import socket 
-
         tab2 = "\t" * 2
         tab3 = "\t" * 3
         line = "--" * 42
-
         info_array = ["@Keyj33k", "1.0.1", "06.04.22", "Python3"]
         links = ["https://github.com/Keyj33k", "https://www.instagram.com/keyjeek/", "nomotikag33n@gmail.com"]
         author_name = info_array[0]
@@ -250,14 +278,12 @@ class Hunter:
         while True:
             print(cld("\n[*] Target (Type 'exit' to exit banner grabber) ~#:", "yellow"))
             target_host = input()
-            
             if target_host == 'exit' or target_host == 'x':
                 os.system("clear")
                 return Hunter()
             
             print(cld("[*] Port (Type 'exit' to exit banner grabber) ~#:", "yellow"))
             target_port = input()
-            
             if target_port == "exit" or target_host == 'x':
                 os.system("clear")
                 return Hunter()
@@ -290,7 +316,6 @@ class Hunter:
                 print(line)
                 print(chr(0xa))
                 input("Press any key ...")
-
             except socket.error as sock_err:
                 os.system("clear")
                 print(cld(f"\nAn error was defined: {sock_err}", "red"))
@@ -308,10 +333,8 @@ class Hunter:
 \t[x] Exit 
         """)
         choice = input("Choice ~#: ")
-        
         if choice == "1":
             hash_value = input("Text ~#: ") 
-            
             if hash_value == 'exit' or hash_value == 'x':
                 return Hunter()     
 
@@ -329,7 +352,6 @@ class Hunter:
                      
         elif choice == '2':
             decode_hash = input("Hash ~#: ") 
-            
             if decode_hash == 'exit' or decode_hash == 'x':
                 print(cld("Exit", "red"))
                 return Hunter()   
@@ -358,8 +380,7 @@ class Hunter:
 
             while True:
                 print(chr(0xa))
-                target_phonenumber = input('PhoneStalk Target ~#: ')
-                        
+                target_phonenumber = input('PhoneStalk Target ~#: ') 
                 if target_phonenumber == 'exit' or target_phonenumber == 'x':
                     print(cld("Exit", "red"))
                     return Hunter()    
@@ -403,18 +424,23 @@ class Hunter:
                 print(chr(0xa))
                 input("Press any key ...")
                 return Hunter()
-            
         PhoneStalk()   
 
 if __name__ == "__main__":
+    magenta = "\033[0;35m"
+    yellow = "\033[0;33m"
+    green = "\033[0;32m"
+    cyan = "\033[0;36m"
+    red = "\033[0;31m"
+    Hunter.conditions()
+    
     def start():
         os.system("clear")
         time_now = dtt.now()
         username = os.getlogin()
         print(cld(f"Welcome {username}. Today is the {time_now}", "yellow"))
         time.sleep(1.25)
-        start_hunter = input("\nDo you want to start hunter? y/n ")
-
+        start_hunter = input(yellow + "\nDo you want to start hunter? (y/n) " + cyan)
         if start_hunter == 'y' or start_hunter == 'Y':
             pass
         elif start_hunter == 'n' or start_hunter == 'N':
@@ -423,7 +449,6 @@ if __name__ == "__main__":
             print(cld("Invalid Input!", "red"))
             input("\nPress any key ...")
             return start()
-
     start()
 
     def hunter_main():
@@ -433,7 +458,7 @@ if __name__ == "__main__":
             global hunter_choice
 
             try:
-                hunter_choice = int(input("Choice ~#: "))
+                hunter_choice = int(input(cyan + "\n~#" + magenta + "$ " + red))
             except ValueError:
                 print(cld("You need to enter an integer!", "red"))
                 input("\nPress any key ...")
@@ -452,14 +477,14 @@ if __name__ == "__main__":
             elif hunter_choice == 6:
                 Hunter.number_tracker()
             elif hunter_choice == 7:
-                
+
                 try:
                     Hunter.subdomain_scanner()
                 except KeyboardInterrupt:
                     print(cld("\nCtrl+C pressed, Exiting ...", "red"))
                     input("\nPress any key ...")
                     return hunter_main()
-                
+
             elif hunter_choice == 99:
                 ex_banner = pfgt.figlet_format("Exit", font = "digital")
                 print(cld(ex_banner, "red"))
@@ -471,6 +496,4 @@ if __name__ == "__main__":
                 print(cld("Invalid Input!", "red"))
                 input("\nPress any key ...")
                 return hunter_main()
-            
     hunter_main()
-    
