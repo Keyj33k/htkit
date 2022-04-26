826f46141699a21b16dbed4dc5b606f4df4a7a8f
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-04-26 12:34:21 +0200

    Update gate.py

--- gate.py
+++ gate.py
@@ -30,7 +30,7 @@ red     = "\033[0;31m"
 #   Twitter :   @keyjeek                  #
 #   Github  :   @keyj33k                  #
 #   Insta   :   @keyjeek                  #
-#   Version :   1.0.8                     #
+#   Version :   1.0.9                     #
 #                                         #
 # # # # # # # # # # # # # # # # # # # # # # 
 
