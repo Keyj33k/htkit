f148ec52f87db8648305be83d428f7f38837ec5d
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-06-27 01:25:35 +0200

    Update phonenumber_lookup.py

--- src/phonenumber_lookup.py
+++ src/phonenumber_lookup.py
@@ -21,6 +21,15 @@ except ImportError:
 
 
     """)
+    
+# # # # # # # # # # # # # # # # # # # # # #
+#                                         #
+#   Author  :   Keyjeek                   #
+#   Contact :   nomotikag33n@gmail.com    #
+#   Github  :   @Keyj33k                  #
+#   Version :   1.1.5                     #
+#                                         #
+# # # # # # # # # # # # # # # # # # # # # #
 
 class PhonenumberWhois:
 
