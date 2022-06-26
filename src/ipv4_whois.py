a2958e7bedc9d74c7c2b8ac7a5e55e2f9e02bfe3
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-06-27 01:24:42 +0200

    Update ipv4_whois.py

--- src/ipv4_whois.py
+++ src/ipv4_whois.py
@@ -19,6 +19,15 @@ except ImportError:
 
 
     """)
+    
+# # # # # # # # # # # # # # # # # # # # # #
+#                                         #
+#   Author  :   Keyjeek                   #
+#   Contact :   nomotikag33n@gmail.com    #
+#   Github  :   @Keyj33k                  #
+#   Version :   1.1.5                     #
+#                                         #
+# # # # # # # # # # # # # # # # # # # # # #
 
 
 class IPv4Lookup:
