089ebbc9fa720d93f062ade37e1943237dfc884d
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-09-15 18:58:30 +0200

    quality adjustments

--- src/infg/url2ip.py
+++ src/infg/url2ip.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import sys
 
 try:
     import socket
@@ -16,18 +15,7 @@ except ImportError:
     "pip3 install -r requirements.txt" 
 
     You will find this file in the req directory.
-
-
     """)
-    
-# # # # # # # # # # # # # # # # # # # # # #
-#                                         #
-#   Author  :   Keyjeek                   #
-#   Contact :   nomotikag33n@gmail.com    #
-#   Github  :   @Keyj33k                  #
-#   Version :   1.1.9                     #
-#                                         #
-# # # # # # # # # # # # # # # # # # # # # #
 
 w = "\033[0;37m"
 g = "\033[0;32m"
@@ -37,10 +25,7 @@ y = "\033[0;33m"
 
 class GetIPv4fromURL:
 
-    def __init__(
-            self,
-            uniformresourcelocator: str
-    ):
+    def __init__(self, uniformresourcelocator: str):
         self.uniformresourcelocator = uniformresourcelocator
 
     def main(self):
@@ -50,19 +35,25 @@ class GetIPv4fromURL:
 
             try:
                 print(f"{r}=" * 70)
-                print(f"{w}[{g}+{w}] IP Address from {self.uniformresourcelocator} {r}->{w} " + socket.gethostbyname(self.uniformresourcelocator))
+                print(
+                    f"{w}[{g}+{w}] IP Address from {self.uniformresourcelocator} {r}->{w} " +
+                    socket.gethostbyname(self.uniformresourcelocator)
+                )
                 print(f"{r}=" * 70)
                 print(chr(0xa))
                 input(f"{w}[{r}*{w}] Press enter key to continue")
+
                 break
             except ValueError:
                 print(f"{w}[{y}-{w}] You need to enter a value like{r}:{w} google.com in example{r}.")
                 print(chr(0xa))
                 input(f"{w}[{r}*{w}] Press enter key to continue")
+
                 break
             except socket.gaierror:
                 print(f"\n{w}[{y}-{w}] You need to enter a value like{r}:{w} google.com in example{r}.")
                 print(chr(0xa))
                 input(f"{w}[{r}*{w}] Press any key to continue")
+
                 break
 
