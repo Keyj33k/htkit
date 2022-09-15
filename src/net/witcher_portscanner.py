6d0db449fe26a657e68d1c719d0104fdc8a31110
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-09-15 18:55:55 +0200

    quality adjustments

--- src/net/witcher_portscanner.py
+++ src/net/witcher_portscanner.py
@@ -15,18 +15,7 @@ except ImportError:
     "pip3 install -r requirements.txt" 
 
     You will find this file in the req directory.
-
-
     """)
-    
-# # # # # # # # # # # # # # # # # # # # # #
-#                                         #
-#   Author  :   Keyjeek                   #
-#   Contact :   nomotikag33n@gmail.com    #
-#   Github  :   @Keyj33k                  #
-#   Version :   1.1.9                     #
-#                                         #
-# # # # # # # # # # # # # # # # # # # # # #
 
 w = "\033[0;37m"
 g = "\033[0;32m"
@@ -36,57 +25,38 @@ y = "\033[0;33m"
 
 class WitcherPortscanner:
 
-    def __init__(
-            self,
-            target_ipv4: str,
-            start_port: int,
-            maximum_port: int
-    ):
+    def __init__(self, target_ipv4: str, start_port: int, maximum_port: int):
         self.start_port = start_port
         self.target_ipv4 = target_ipv4
         self.maximum_port = maximum_port
 
     def main(self):
         while True:
-            if self.target_ipv4 == 'x' or self.target_ipv4 == 'exit':
+            if self.target_ipv4 == 'x' or self.target_ipv4 == "exit":
                 break
             elif self.maximum_port == 0:
                 break
 
             print(f"\n{w}[{r}*{w}] Started scanning at{r}:{w}\t{dtt.now()}")
             print(f"{r}=" * 70)
-            
             time_start = dtt.now()
-            
             print(f"{w}Protocol\tPort\t\tStatus\t Service\n" + "-" * 70)
 
             try:
-                for target in range(
-                        self.start_port,
-                        self.maximum_port + 1
-                ):
-                    socket_sock = socket.socket(
-                        socket.AF_INET,
-                        socket.SOCK_STREAM
-                    )
-                    final_result = socket_sock.connect_ex((
-                        self.target_ipv4,
-                        target
-                    ))
-                    socket_sock.settimeout(5)
+                for target_port in range(self.start_port, self.maximum_port + 1):
+                    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_sock:
+                        final_result = socket_sock.connect_ex((self.target_ipv4, target_port))
+                        socket_sock.settimeout(5)
                     
-                    if final_result == 0:
-                        try:
-                            print(f"TCP\t\t{target}  \t\topen\t", socket.getservbyport(target))
-                        except:
-                            print(f"TCP\t\t{target}  \t\topen\t Unknown")
+                        if final_result == 0:
+                            try:
+                                print(f"TCP\t\t{target_port}  \t\topen\t", socket.getservbyport(target_port))
+                            except:
+                                print(f"TCP\t\t{target_port}  \t\topen\t Unknown")
                             
                     socket_sock.close()
             except socket.error as socket_error:
-                print(cld(
-                    socket_error,
-                    "red"
-                ))
+                print(cld(socket_error, "red"))
                 break
             except KeyboardInterrupt:
                 print(f"\n{w}[{y}-{w}] Ctrl+C pressed{r}.{w} Exit{r}.")
@@ -99,4 +69,5 @@ class WitcherPortscanner:
             print(f"{w}[{r}*{w}] Scanner done in {needed_time}!")
             print(chr(0xa))
             input(f"{w}[{r}*{w}] Press enter key to continue")
+
             break
