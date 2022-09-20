f90a1c9ffd36a95f7fb355e06d67a3d6fc40fe53
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-09-20 18:12:52 +0200

    refactored witcher_portscanner

--- src/net/witcher_portscanner.py
+++ src/net/witcher_portscanner.py
@@ -3,7 +3,7 @@
 try:
     from datetime import datetime as dtt
     from termcolor import colored as cld
-    import socket
+    from socket import error, AF_INET, SOCK_STREAM, socket, getservbyport
 except ImportError:
     raise RuntimeError("""
     Oops,
@@ -43,18 +43,18 @@ class WitcherPortscanner:
 
             try:
                 for target_port in range(self.start_port, self.maximum_port + 1):
-                    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket_sock:
+                    with socket(AF_INET, SOCK_STREAM) as socket_sock:
                         final_result = socket_sock.connect_ex((self.target_ipv4, target_port))
                         socket_sock.settimeout(5)
                     
                         if final_result == 0:
                             try:
-                                print(f"TCP\t\t{target_port}  \t\topen\t", socket.getservbyport(target_port))
+                                print(f"TCP\t\t{target_port}  \t\topen\t", getservbyport(target_port))
                             except OSError:
                                 print(f"TCP\t\t{target_port}  \t\topen\t Unknown")
                             
                     socket_sock.close()
-            except socket.error as socket_error:
+            except error as socket_error:
                 print(cld(socket_error, "red"))
                 break
             except KeyboardInterrupt:
