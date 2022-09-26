022586235b5b551e769b72661a40463f7fc63f87
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-09-26 21:11:53 +0200

    bugfix: missing input checks
    added loop for runtime controlling

--- src/net/ipsweep.py
+++ src/net/ipsweep.py
@@ -5,7 +5,6 @@ try:
     from subprocess import check_output
     from pyfiglet import figlet_format
     from datetime import datetime
-    import os
 except ImportError:
     raise RuntimeError("""
     Oops,
@@ -34,28 +33,46 @@ class IPv4Sweep:
         self.ipv4_address = ipv4_address
 
     def get_status(self):
-        print(f"\n{w}[{r}*{w}] Started scanning at{r}:{w}\t{datetime.now()}")
-        print(f"{r}=" * 70)
-
-        start = datetime.now()
-
-        for icmp_request in range(self.min_range, self.max_range + 1):
-            ip_address = f"{self.ipv4_address}.{str(icmp_request)}"
-            try:
-                check_output(["ping", "-c", self.ping_count, ip_address])
-                print(f"{w}[{g}+{w}] Host {ip_address} is reachable{r}!")
-            except KeyboardInterrupt:
-                print(f"{w}[{y}-{w}] You pressed Ctrl+C{r}.{w} Interrupted!")
+        while True:
+            address_split = self.ipv4_address.split(".")
+            if len(self.ipv4_address.split(".")) != 3:
+                print(f"\n{w}[{y}-{w}] Only the first three octets are allowed{r}!")
                 break
-            except CalledProcessError:
-                print(f"{w}[{y}-{w}] Host {ip_address} is not reachable{r}!")
+            elif self.min_range <= 0 or self.min_range >= 252:
+                print(f"\n{w}[{y}-{w}] Invalid min host config!")
+                break
+            elif self.max_range <= 0 or self.max_range >= 253:
+                print(f"\n{w}[{y}-{w}] Invalid max host config!")
+                break
+            elif int(address_split[0]) <= 0 or int(address_split[0]) >= 253:
+                print(f"\n{w}[{y}-{w}] Octet {address_split[0]} is invalid{r}!")
+                break
+            elif int(address_split[1]) <= 0 or int(address_split[1]) >= 253:
+                print(f"\n{w}[{y}-{w}] Octet {address_split[1]} is invalid{r}!")
+                break
+            elif int(address_split[2]) <= 0 or int(address_split[2]) >= 253:
+                print(f"\n{w}[{y}-{w}] Octet {address_split[2]} is invalid{r}!")
+                break
 
-        end = datetime.now()
+            print(f"\n{w}[{r}*{w}] Started scanning at{r}:{w}\t{datetime.now()}")
+            print(f"{r}=" * 70)
 
-        print(f"{r}=" * 70)
-        print(f"{w}[{r}*{w}] Scanner done in {end - start}{r}!")
-        print(chr(0xa))
-        input(f"{w}[{r}*{w}] Press enter key to continue")
+            start = datetime.now()
 
+            for icmp_request in range(self.min_range, self.max_range + 1):
+                ip_address = f"{self.ipv4_address}.{str(icmp_request)}"
+                try:
+                    check_output(["ping", "-c", self.ping_count, ip_address])
+                    print(f"{w}[{g}+{w}] Host {ip_address} is reachable{r}!")
+                except KeyboardInterrupt:
+                    print(f"{w}[{y}-{w}] You pressed Ctrl+C{r}.{w} Interrupted!")
+                    break
+                except CalledProcessError:
+                    print(f"{w}[{y}-{w}] Host {ip_address} is not reachable{r}!")
 
-
+            end = datetime.now()
+            print(f"{r}=" * 70)
+            print(f"{w}[{r}*{w}] Scanner done in {end - start}{r}!")
+            print(chr(0xa))
+            input(f"{w}[{r}*{w}] Press enter key to continue")
+            break
