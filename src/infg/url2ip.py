bd06105eecfb175999d1460e192371b879957b70
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-10-06 12:12:49 +0200

    url2ip: refactored

--- src/infg/url2ip.py
+++ src/infg/url2ip.py
@@ -3,7 +3,6 @@
 try:
     from socket import gethostbyname, gaierror
     from termcolor import colored as cld
-    from pyfiglet import figlet_format
 except ImportError:
     raise RuntimeError("""
     Oops,
@@ -22,35 +21,24 @@ g = "\033[0;32m"
 r = "\033[0;31m"
 y = "\033[0;33m"
 
+def error_message():
+    print(f"{w}[{y}-{w}] You need to enter a value like{r}:{w} google.com in example{r}.\n{chr(0xa)}")
+    input(f"{w}[{r}*{w}] Press enter key to continue")
 
 class GetIPv4fromURL:
     def __init__(self, uniformresourcelocator: str):
         self.uniformresourcelocator = uniformresourcelocator
 
-    @staticmethod
-    def error_message():
-        print(f"{w}[{y}-{w}] You need to enter a value like{r}:{w} google.com in example{r}.")
-        print(chr(0xa))
-        input(f"{w}[{r}*{w}] Press enter key to continue")
-
     def main(self):
         while True:
-            if self.uniformresourcelocator == 'x' or self.uniformresourcelocator == 'exit':
-                break
+            if self.uniformresourcelocator == 'x' or self.uniformresourcelocator == 'exit': break
 
             try:
-                print(f"{r}=" * 70)
                 print(
-                    f"{w}[{g}+{w}] IP Address from {self.uniformresourcelocator} {r}->{w} " + \
-                    f"{gethostbyname(self.uniformresourcelocator)}"
-                )
-                print(f"{r}=" * 70)
-                print(chr(0xa))
+                    f"{'=' * 70}\n{w}[{g}+{w}] IP Address from {self.uniformresourcelocator}"
+                    f" {r}->{w} " + f"{gethostbyname(self.uniformresourcelocator)}\n{'=' * 70}\n{chr(0xa)}")
                 input(f"{w}[{r}*{w}] Press enter key to continue")
                 break
-            except ValueError:
-                GetIPv4fromURL.error_message()
-                break
-            except gaierror:
-                GetIPv4fromURL.error_message()
+            except (ValueError, gaierror):
+                error_message()
                 break
