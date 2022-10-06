69ea2ccf15d977256866ce182acbf3e0b20da02f
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-10-06 10:52:57 +0200

    refactored email extractor

--- src/infg/email_extractor.py
+++ src/infg/email_extractor.py
@@ -29,33 +29,30 @@ class EmailExtractor:
         self.uniformresourcelocator = uniformresourcelocator
 
     def extract_mail_address(self):
-        email_address_regex = """(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\n\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
-        start_time = datetime.now()
+        email_address_regex = ("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&"
+                               "'*+/=?^_`{|}~-]+)*|(?:[\x01-\x08\x0b\x0c\x0e-"
+                               "\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"
+                               ")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]"
+                               "*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?"
+                               "[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])"
+                               "|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-"
+                               "\x5a\x53-\x7f]|\\[\n\x01-\x09\x0b\x0c\x0e-\x7f])+)\])")
 
         while True:
-            print(f"\n{w}[{r}*{w}] Started scanning at{r}:{w}\t{datetime.now()}")
-            print(f"{r}=" * 70)
+            start_time = datetime.now()
+            print(f"{'=' * 70}\n{w}[{r}*{w}] Started scanning at{r}:{w}\t{start_time}")
 
             try:
-                content = requests.get(self.uniformresourcelocator).content
+                sc_content = requests.get(self.uniformresourcelocator).content
+                for url in re.finditer(email_address_regex, sc_content.decode()):
+                    print(f"{w}[{g}+{w}] Email address found:\t{url.group()}")
 
-                for url in re.finditer(email_address_regex, content.decode()):
-                    addr = url.group()
-                    print(f"{w}[{g}+{w}] Email address found!\t{addr}")
-
+                print(f"{r}{'=' * 70}\n{w}[{r}*{w}] Scanner done in {datetime.now() - start_time}!")
+                input(f"{chr(0xa)}\n{w}[{r}*{w}] Press enter key to continue")
                 break
             except requests.exceptions.MissingSchema:
                 print(f"{w}[{y}-{w}] Invalid format{r}.{w} Did you mean 'http://{self.uniformresourcelocator}/'{r}?{w}")
-
                 break
             except Exception as excerr:
                 print(excerr)
-
                 break
-
-        end_time = datetime.now()
-
-        print(f"{r}=" * 70)
-        print(f"{w}[{r}*{w}] Scanner done in {end_time - start_time}!")
-        print(chr(0xa))
-        input(f"{w}[{r}*{w}] Press enter key to continue")
