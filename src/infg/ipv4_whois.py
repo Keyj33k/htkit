f9dee681461ea2a54dcb3e55e3c00a03da695942
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-10-06 11:17:40 +0200

    refactored config checks

--- src/infg/ipv4_whois.py
+++ src/infg/ipv4_whois.py
@@ -22,6 +22,16 @@ g = "\033[0;32m"
 r = "\033[0;31m"
 y = "\033[0;33m"
 
+def config_check(ip_addr: str):
+    split_address = ip_addr.split(".")
+    if len(split_address) != 4:
+        print(f"\n{w}[{y}-{w}] You need to enter a valid 32 bit address{r}!")
+        return False
+
+    for octet in range(4):
+        if int(split_address[octet]) <= 0 or int(split_address[octet]) >= 253:
+            print(f"\n{w}[{y}-{w}] Octet {octet + 1} ( {split_address[octet]} ) is invalid{r}!")
+            return False
 
 class IPv4Lookup:
     def __init__(self, ipv4_address: str):
@@ -29,41 +39,19 @@ class IPv4Lookup:
 
     def main(self):
         while True:
-            split_address = self.ipv4_address.split(".")
-            if len(split_address) != 4:
-                print(f"\n{w}[{y}-{w}] You need to enter a valid 32 bit address{r}!")
-                break
-            elif int(split_address[0]) <= 0 or int(split_address[0]) >= 253:
-                print(f"\n{w}[{y}-{w}] Octet 1 ( {split_address[0]} ) is invalid{r}!")
-                break
-            elif int(split_address[1]) <= 0 or int(split_address[1]) >= 253:
-                print(f"\n{w}[{y}-{w}] Octet 2 ( {split_address[1]} ) is invalid{r}!")
-                break
-            elif int(split_address[2]) <= 0 or int(split_address[2]) >= 253:
-                print(f"\n{w}[{y}-{w}] Octet 3 ( {split_address[2]} ) is invalid{r}!")
-                break
-            elif int(split_address[3]) <= 0 or int(split_address[3]) >= 253:
-                print(f"\n{w}[{y}-{w}] Octet 4 ( {split_address[3]} ) is invalid{r}!")
-                break
-
+            if config_check(self.ipv4_address) is False: break
             time_start = dtt.now()
             print(f"\n{w}[{r}*{w}] Results{r}:\n{'=' * 70}")
 
             try:
                 for lookup in post("http://ip-api.com/batch", json=[{"query": self.ipv4_address}]).json():
-                    for k, j in lookup.items():
-                        print(f"{w}[{g}+{w}] " + k, j)
+                    for category, result in lookup.items():
+                        print(f"{w}[{g}+{w}] {category}: {result}")
 
-                time_stop = dtt.now()
-                time_result = time_stop - time_start
-
-                print(f"{r}=" * 70)
-                print(f"{w}[{r}*{w}] Scanner done in {time_result}!")
-                print(chr(0xa))
-                input(f"{w}[{r}*{w}] Press enter key to continue")
+                print(f"{r}{'=' * 70}\n{w}[{r}*{w}] Scanner done in {dtt.now() - time_start}!")
+                input(f"{chr(0xa)}\n{w}[{r}*{w}] Press enter key to continue")
                 break
             except RequestException as error:
                 print(cld(str(error), "red"))
                 input(f"{w}[{g}+{w}] Press enter key to continue")
                 break
-
