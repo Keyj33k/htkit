e646cc98d6918d160da2394f79276e9f47a7d81f
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-10-06 10:52:15 +0200

    refactored email extractor

--- src/infg/http_header.py
+++ src/infg/http_header.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 
 try:
-    from requests import get
-    from requests.exceptions import ConnectionError
+    from datetime import datetime
+    import requests
+    import re
 except ImportError:
     raise RuntimeError("""
     Oops,
-
+    
     this tool uses important modules, which don't seem to be 
     installed at the moment.
-
+    
     Use the requirements file and this command:
     "pip3 install -r requirements.txt" 
-
+    
     You will find this file in the req directory.
     """)
 
@@ -22,25 +23,36 @@ r = "\033[0;31m"
 y = "\033[0;33m"
 
 
-class HTTPHeader:
+class EmailExtractor:
+
     def __init__(self, uniformresourcelocator: str):
         self.uniformresourcelocator = uniformresourcelocator
 
-    def main(self):
-        while True:
-            if self.uniformresourcelocator == 'x' or self.uniformresourcelocator == 'exit':
-                break
+    def extract_mail_address(self):
+        email_address_regex = ("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&"
+                               "'*+/=?^_`{|}~-]+)*|(?:[\x01-\x08\x0b\x0c\x0e-"
+                               "\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"
+                               ")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]"
+                               "*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?"
+                               "[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])"
+                               "|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-"
+                               "\x5a\x53-\x7f]|\\[\n\x01-\x09\x0b\x0c\x0e-\x7f])+)\])")
 
-            print(f"\n{w}[{g}+{w}] Result{r}:\n{'=' * 70}{w}")
+        while True:
+            start_time = datetime.now()
+            print(f"{'=' * 70}\n{w}[{r}*{w}] Started scanning at{r}:{w}\t{start_time}")
 
             try:
-                for category, result in get(f"http://{self.uniformresourcelocator}").headers.items():
-                    print(f"{category}: {result}")
+                sc_content = requests.get(self.uniformresourcelocator).content
+                for url in re.finditer(email_address_regex, sc_content.decode()):
+                    print(f"{w}[{g}+{w}] Email address found:\t{url.group()}")
 
-                print(f"{r}{'=' * 70}\n{chr(0xa)}")
-                input(f"{w}[{r}*{w}] Press enter key to continue")
+                print(f"{r}{'=' * 70}\n{w}[{r}*{w}] Scanner done in {datetime.now() - start_time}!")
+                input(f"{chr(0xa)}\n{w}[{r}*{w}] Press enter key to continue")
                 break
-            except ConnectionError:
-                print(f"{w}[{y}-{w}] Failed getting header from {self.uniformresourcelocator}{r}!\n{chr(0xa)}")
-                input(f"{w}[{r}*{w}] Press enter key to continue")
+            except requests.exceptions.MissingSchema:
+                print(f"{w}[{y}-{w}] Invalid format{r}.{w} Did you mean 'http://{self.uniformresourcelocator}/'{r}?{w}")
+                break
+            except Exception as excerr:
+                print(excerr)
                 break
