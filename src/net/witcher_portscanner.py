94f4257d714a33f6964f9a70e8c84198de62914e
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-10-07 15:22:23 +0200

    removed config check

--- src/net/witcher_portscanner.py
+++ src/net/witcher_portscanner.py
@@ -4,6 +4,7 @@ try:
     from datetime import datetime as dtt
     from termcolor import colored as cld
     from socket import error, AF_INET, SOCK_STREAM, socket, getservbyport
+    from src.conf_checks.conf import Conf
 except ImportError:
     raise RuntimeError("""
     Oops,
@@ -22,24 +23,19 @@ g = "\033[0;32m"
 r = "\033[0;31m"
 y = "\033[0;33m"
 
-def output(port: int, service: str):
-    print(f"TCP\t\t{port}  \t\topen\t{service}")
-
 class WitcherPortscanner:
     def __init__(self, target_ipv4: str, start_port: int, maximum_port: int):
         self.start_port = start_port
         self.target_ipv4 = target_ipv4
         self.maximum_port = maximum_port
 
+    @staticmethod
+    def output(port: int, service: str):
+        print(f"TCP\t\t{port}  \t\topen\t{service}")
+
     def main(self):
         while True:
-            if self.start_port <= 0 or self.start_port >= 65534:
-                print(f"\n{w}[{y}-{w}] Port {self.start_port} is invalid{r}!")
-                break
-            elif self.maximum_port <= 1 or self.maximum_port >= 65535:
-                print(f"\n{w}[{y}-{w}] Port {self.maximum_port} is invalid{r}!")
-                break
-
+            if Conf.ports(self.start_port, self.maximum_port) is False: break
             time_start = dtt.now()
             print(f"\n{w}[{r}*{w}] Started scanning at{r}:{w}\t{time_start}\n{r}{'=' * 70}\n"
                   f"{w}Protocol\tPort\t\tStatus\t Service\n{'-' * 70}")
@@ -50,9 +46,9 @@ class WitcherPortscanner:
                         socket_sock.settimeout(5)
                         if socket_sock.connect_ex((self.target_ipv4, target_port)) == 0:
                             try:
-                                output(target_port, getservbyport(target_port))
+                                WitcherPortscanner.output(target_port, getservbyport(target_port))
                             except OSError:
-                                output(target_port, "unknown")
+                                WitcherPortscanner.output(target_port, "unknown")
                 print(f"{r}{'=' * 70}\n{w}[{r}*{w}] Scanner done in {dtt.now() - time_start}!\n{chr(0xa)}")
                 input(f"{w}[{r}*{w}] Press enter key to continue")
                 break
