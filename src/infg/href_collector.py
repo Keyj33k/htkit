4a6b4033864b177eb550c0f2cd54eb081a735a17
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-10-09 18:57:50 +0200

    renamed file and redesigned output

--- src/infg/href_collector.py
+++ src/infg/href_collector.py
@@ -6,6 +6,7 @@ try:
     from bs4 import BeautifulSoup
     from requests import get
     from requests.exceptions import MissingSchema, ConnectionError, InvalidSchema
+    from datetime import datetime
 except ImportError:
     raise RuntimeError("""
     Oops,
@@ -19,26 +20,27 @@ except ImportError:
     You will find this file in the req directory.
     """)
 
-w = "\033[0;37m"
-g = "\033[0;32m"
-r = "\033[0;31m"
-y = "\033[0;33m"
+W = "\033[0;37m"
+G = "\033[0;32m"
+R = "\033[0;31m"
+Y = "\033[0;33m"
 
-class LinkCollector:
+class HREFCollector:
     def __init__(self, url: str):
         self.url = url
 
     def main(self):
         while True:
             if self.url == 'x' or self.url == 'exit': break
-            print(f"\n{w}[{r}*{w}] Results:\n{r}{'=' * 70}")
+            print(f"{W}[{R}*{W}] Sending GET requests {R}...")
+            start = datetime.now()
             for collected_links in BeautifulSoup(get(f"http://{self.url}/").text, "html.parser").find_all("a"):
                 try:
-                    print(f"{w}[{g}+{w}] Href found {r}->{w} {collected_links.get('href')} "
-                          f"{r}[{w} {get(collected_links.get('href')).status_code} {r}]{w}")
-                except (InvalidSchema, MissingSchema):
-                    print(f"{w}[{g}+{w}] Href found {r}->{w} {collected_links.get('href')}")
 
-            print(f"{r}{'=' * 70}\n{chr(0xa)}")
-            input(f"{w}[{r}*{w}] Press enter key to continue")
+                    print(f"{W}[{G}+{W}] Href found{R}:{W} {collected_links.get('href')} "
+                          f"{R}[{W} {get(collected_links.get('href')).status_code} {R}]{W}")
+                except (InvalidSchema, MissingSchema):
+                    print(f"{W}[{G}+{W}] Href found{R}:{W} {collected_links.get('href')}")
+            print(f"{W}[{R}*{W}] Done{R},{W} runtime{R}:{W} {datetime.now() - start}")
+            input(f"{W}[{R}*{W}] Press enter key to continue")
             break
