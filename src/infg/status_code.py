c34d991222ef6a459c450c4da99ecc2e5d3a5501
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-10-09 18:59:49 +0200

    redesigned output

--- src/infg/status_code.py
+++ src/infg/status_code.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 try:
-    from termcolor import colored as cld
     import urllib3
     from time import sleep
+    from pwd import getpwuid
+    from os import getuid
 except ImportError:
     raise RuntimeError("""
     Oops,
@@ -17,27 +18,26 @@ except ImportError:
     You will find this file in the req directory.
     """)
 
-w = "\033[0;37m"
-g = "\033[0;32m"
-r = "\033[0;31m"
-y = "\033[0;33m"
-
+W = "\033[0;37m"
+G = "\033[0;32m"
+R = "\033[0;31m"
+Y = "\033[0;33m"
 
 class RemoteServerStatusCode:
-    def __init__(self, uniformresourcelocator: str):
-        self.uniformresourcelocator = uniformresourcelocator
+    def __init__(self, url: str):
+        self.url = url
 
-    def get_code(self):
+    def main(self):
         while True:
             try:
-                send_request = urllib3.PoolManager().request("GET", self.uniformresourcelocator)
-                print(f"\n{w}Result:\n{r}{'=' * 70}\n"
-                      f"{w}[{g}+{w}] {self.uniformresourcelocator} status code {r}->{w} {send_request.status}")
-                input(f"{r}{'=' * 70}\n{chr(0xa)}\n{w}[{r}*{w}] Press enter key to continue")
-                break
-            except KeyboardInterrupt:
-                print(f"\n{w}[{r}*{w}] You pressed Ctrl+C{r}.{w} Exit{r}.")
+                print(f"{W}[{R}*{W}] Sending GET request {R}...")
+                sleep(0.25)
+                print(f"{W}[{G}+{W}] {self.url}{R}:{W} {urllib3.PoolManager().request('GET', self.url).status}")
+                input(f"{W}[{R}*{W}] Press enter key to continue")
                 break
             except Exception as excerr:
-                print(cld(str(excerr), "red"))
+                print(f"{W}[{Y}-{W}] Error{R}:{W} {excerr}")
+                break
+            except KeyboardInterrupt:
+                print(f"\n{W}[{R}*{W}] You pressed Ctrl{R}+{W}C{R}.{W} Exit{R}.")
                 break
