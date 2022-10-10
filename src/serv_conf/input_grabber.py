02040b4fe6d08931a4277b5b837330a22644a5d2
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-10-10 21:53:17 +0200

    replaced name in input helper

--- src/serv_conf/input_grabber.py
+++ src/serv_conf/input_grabber.py
@@ -29,49 +29,49 @@ class InputGrabber:
     @staticmethod
     def grab_wordlist(service: str):
         while True:
-            wordlist = str(input(f"{W}[{R}*{W}] {W}{USERNAME}{R}@{W}Hunter{R},{W} Wordlist "
+            wordlist = str(input(f"{W}[{R}*{W}] {W}{USERNAME}{R}@{W}htkit{R},{W} Wordlist "
                                  f"{R}({W} Enter For Default Wordlist {R}) >>{Y} "))
             if exit_str_err(wordlist, service) is True: return wordlist
 
     @staticmethod
     def grab_address(service: str):
         while True:
-            address = str(input(f"{W}[{R}*{W}] {W}{USERNAME}{R}@{W}Hunter{R},{W} Address{R} >>{Y} "))
+            address = str(input(f"{W}[{R}*{W}] {W}{USERNAME}{R}@{W}htkit{R},{W} Address{R} >>{Y} "))
             if exit_str_err(address, service) is True: return address
 
     @staticmethod
     def grab_ipv4(service: str):
         while True:
-            ipv4 = str(input(f"{W}[{R}*{W}] {W}{USERNAME}{R}@{W}Hunter{R},{W} IPv4{R} >>{Y} "))
+            ipv4 = str(input(f"{W}[{R}*{W}] {W}{USERNAME}{R}@{W}htkit{R},{W} IPv4{R} >>{Y} "))
             if exit_str_err(ipv4, service) is True: return ipv4
 
     @staticmethod
     def grab_phonenumber(service: str):
         while True:
-            phonenumber = str(input(f"{W}[{R}*{W}] {W}{USERNAME}{R}@{W}Hunter{R},{W} Phonenumber{R} >>{Y} "))
+            phonenumber = str(input(f"{W}[{R}*{W}] {W}{USERNAME}{R}@{W}htkit{R},{W} Phonenumber{R} >>{Y} "))
             if exit_str_err(phonenumber, service) is True: return phonenumber
 
     @staticmethod
     def grab_url(service: str):
         while True:
-            url = str(input(f"{W}[{R}*{W}] {W}{USERNAME}{R}@{W}Hunter{R},{W} URL{R} >>{Y} "))
+            url = str(input(f"{W}[{R}*{W}] {W}{USERNAME}{R}@{W}htkit{R},{W} URL{R} >>{Y} "))
             if exit_str_err(url, service) is True: return url
 
     @staticmethod
     def grab_int(service: str, category: str):
         while True:
-            value = int(input(f"{W}[{R}*{W}] {W}{USERNAME}{R}@{W}Hunter{R},{W} {category}{R} >>{Y} "))
+            value = int(input(f"{W}[{R}*{W}] {W}{USERNAME}{R}@{W}htkit{R},{W} {category}{R} >>{Y} "))
             if exit_int_err(value, service) is True: return value
 
     @staticmethod
     def grab_sec_port(service: str):
         while True:
-            last_port = int(input(f"{W}[{R}*{W}] {W}{USERNAME}{R}@{W}Hunter{R},{W} Max{R}.{W} Port{R} >>{Y} "))
+            last_port = int(input(f"{W}[{R}*{W}] {W}{USERNAME}{R}@{W}htkit{R},{W} Max{R}.{W} Port{R} >>{Y} "))
             if exit_int_err(last_port, service) is True: return last_port
 
     @staticmethod
     def grab_ip_val(service: str):
         while True:
-            value = str(input(f"{W}[{R}*{W}] {W}{USERNAME}{R}@{W}Hunter{R},"
+            value = str(input(f"{W}[{R}*{W}] {W}{USERNAME}{R}@{W}htkit{R},"
                               f"{W} IPv4 Address {R}({W} First Three Octets Only {R}) >>{Y} "))
             if exit_str_err(value, service) is True: return value
