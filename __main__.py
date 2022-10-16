56b84cb858c92918b5f95acd85d440c5bb972b7f
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-10-16 16:32:25 +0200

    removed details

--- __main__.py
+++ __main__.py
@@ -1,13 +1,3 @@
 #!/usr/bin/env python3
-
-# # # # # # # # # # # # # # # # # # # # # #
-#                                         #
-#   Author  :   Keyjeek                   #
-#   Contact :   K3yjeek@proton.me         #
-#   Github  :   @Keyj33k                  #
-#   Version :   1.1.14                    #
-#                                         #
-# # # # # # # # # # # # # # # # # # # # # #
-
 from src import htkit
 htkit.entry_point()
