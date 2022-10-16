a7ef0cfd61e17f05485d4925f496414d44edd223
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-10-16 10:17:53 +0200

    added coloring import and deleted unused vars

--- htkit.py
+++ htkit.py
@@ -22,6 +22,8 @@ try:
     from src.system.system_inf import SystemInformations
 
     from src.serv_conf.input_grabber import InputGrabber
+    
+    from src.colors.coloring import R, W, Y
 
     from termcolor import colored as cld
     from pyfiglet import figlet_format
@@ -50,11 +52,6 @@ except ImportError:
 #   Version :   1.1.13                    #
 #                                         #
 # # # # # # # # # # # # # # # # # # # # # #
-
-Y = "\033[0;33m"
-G = "\033[0;32m"
-W = "\033[0;37m"
-R = "\033[0;31m"
 
 USERNAME = getpwuid(getuid())[0]
 
