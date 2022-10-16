f5e3c5e12ad85abf6fc3099d68eea0204cf412ac
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-10-16 11:48:44 +0200

    added coloring import, deleted unused vars and removed class shorts

--- src/infg/phonenumber_lookup.py
+++ src/infg/phonenumber_lookup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 try:
-    from datetime import datetime as dtt
-    from phonenumbers import timezone as tz
-    from phonenumbers import geocoder as gc
-    from phonenumbers import carrier as cr
-    import phonenumbers as pnmb
+    from datetime import datetime
+    from phonenumbers import timezone
+    from phonenumbers import geocoder
+    from phonenumbers import carrier
+    import phonenumbers
     from time import sleep
+    from src.colors.coloring import W, R, Y, G
 except ImportError:
     raise RuntimeError("""
     Oops,
@@ -20,33 +21,25 @@ except ImportError:
     You will find this file in the req directory.
     """)
 
-W = "\033[0;37m"
-G = "\033[0;32m"
-R = "\033[0;31m"
-Y = "\033[0;33m"
-
-
 class PhonenumberWhois:
     def __init__(self, target_phonenumber: str):
         self.target_phonenumber = target_phonenumber
 
     def main(self):
         while True:
-            time_start = dtt.now()
+            time_start = datetime.now()
             print(f"{W}[{R}*{W}] Collecting values {R}...")
 
             try:
-                phonenumber_val = pnmb.is_valid_number(pnmb.parse(self.target_phonenumber))
-                national = pnmb.format_number(pnmb.parse(self.target_phonenumber), pnmb.PhoneNumberFormat.NATIONAL)
-                international = pnmb.format_number(
-                    pnmb.parse(self.target_phonenumber),
-                    pnmb.PhoneNumberFormat.INTERNATIONAL
-                )
-                phonenumbers_ = pnmb.parse(self.target_phonenumber, None)
-                final_timezone = tz.time_zones_for_number(phonenumbers_)
-                final_phonenumbers_location = gc.description_for_number(phonenumbers_, "en")
-                final_phonenumbers_provider = cr.name_for_number(phonenumbers_, "en")
-
+                phonenumber_val = phonenumbers.is_valid_number(phonenumbers.parse(self.target_phonenumber))
+                national = phonenumbers.format_number(phonenumbers.parse(self.target_phonenumber),
+                                                      phonenumbers.PhoneNumberFormat.NATIONAL)
+                international = phonenumbers.format_number(phonenumbers.parse(self.target_phonenumber),
+                                                           phonenumbers.PhoneNumberFormat.INTERNATIONAL)
+                phonenumbers_ = phonenumbers.parse(self.target_phonenumber, None)
+                final_timezone = timezone.time_zones_for_number(phonenumbers_)
+                final_phonenumbers_location = geocoder.description_for_number(phonenumbers_, "en")
+                final_phonenumbers_provider = carrier.name_for_number(phonenumbers_, "en")
                 output = [
                     f"Validation{R}:{W} {phonenumber_val}",
                     f"National{R}:{W} {national}",
@@ -60,7 +53,7 @@ class PhonenumberWhois:
                     sleep(0.25)
                     print(f"{W}[{G}+{W}] {results}")
 
-                print(f"{W}[{R}*{W}] Done, runtime{R}:{W} {dtt.now() - time_start}")
+                print(f"{W}[{R}*{W}] Done, runtime{R}:{W} {datetime.now() - time_start}")
                 input(f"{W}[{R}*{W}] Press enter key to continue")
                 break
             except Exception as error:
