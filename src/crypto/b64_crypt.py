c087d7c7d016a2abdd7c898562e29ae159d0d7d2
Keyjeek <88333825+Keyj33k@users.noreply.github.com>
2022-10-16 10:29:54 +0200

    changed if to match and added import for coloring

--- src/crypto/b64_crypt.py
+++ src/crypto/b64_crypt.py
@@ -6,6 +6,7 @@ try:
     from pwd import getpwuid
     from os import getuid
     import base64 as b64
+    from src.colors.coloring import W, R, Y
 except ImportError:
     raise RuntimeError("""
     Oops,
@@ -19,11 +20,6 @@ except ImportError:
     You will find this file in the req directory.
     """)
 
-W = "\033[0;37m"
-R = "\033[0;31m"
-Y = "\033[0;33m"
-G = "\033[0;32m"
-
 def build_result(value: str, to_crypt):
     try:
         print(f"{W}[{G}+{W}] {value}{R}:{W} {to_crypt}")
@@ -35,7 +31,6 @@ def build_result(value: str, to_crypt):
 USERNAME = getpwuid(getuid())[0]
 
 class Crypt:
-    
     @staticmethod
     def main():
         while True:
@@ -45,16 +40,16 @@ class Crypt:
                   f"\t{W}[{R}x{W}] Exit")
             
             b64_choice = input(f"\n{W}[{R}*{W}] Choice {R}>>{Y} ")
+            if b64_choice == "exit" or b64_choice == 'x': break
             
-            if b64_choice == "1":
-                value_to_encrypt = str(input(f"{W}[{R}*{W}] {W}{USERNAME}{R}@{W}Hunter{R},{W} Value{R} >>{Y} "))
-                if value_to_encrypt == "exit" or value_to_encrypt == 'x': break
-                build_result(value_to_encrypt, b64.b64encode(value_to_encrypt.encode('ascii')).decode('ascii'))
-            elif b64_choice == "2":
-                hash_to_decrypt = str(input(f"{W}[{R}*{W}] {W}{USERNAME}{R}@{W}Hunter{R},{W} Value{R} >>{Y} "))
-                if hash_to_decrypt == "exit" or hash_to_decrypt == 'x': break
-                build_result(hash_to_decrypt, b64.b64decode(hash_to_decrypt.encode('ascii')).decode('ascii'))
-            elif b64_choice == "exit" or b64_choice == 'x':
-                break
-            else:
-                print(cld("Invalid Input!", "red"))
+            match b64_choice: 
+                case "1":
+                    value_to_encrypt = str(input(f"{W}[{R}*{W}] {W}{USERNAME}{R}@{W}Hunter{R},{W} Value{R} >>{Y} "))
+                    if value_to_encrypt == "exit" or value_to_encrypt == 'x': break
+                    build_result(value_to_encrypt, b64.b64encode(value_to_encrypt.encode('ascii')).decode('ascii'))
+                case "2":
+                    hash_to_decrypt = str(input(f"{W}[{R}*{W}] {W}{USERNAME}{R}@{W}Hunter{R},{W} Value{R} >>{Y} "))
+                    if hash_to_decrypt == "exit" or hash_to_decrypt == 'x': break
+                    build_result(hash_to_decrypt, b64.b64decode(hash_to_decrypt.encode('ascii')).decode('ascii'))
+                case _:
+                    print(cld("Invalid Input!", "red"))
